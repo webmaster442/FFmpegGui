bf9481c82187c3537ab703c97af8dda4557546ab
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-03-09 19:45:07 +0100

    Video scale input

---
+++ FFmpeg.Gui/Controls/TextToIntConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace FFmpeg.Gui.Controls
+{
+    internal class TextToIntConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                if (int.TryParse(str, out int parsed))
+                    return parsed;
+                else
+                    return -1;
+            }
+            return -1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ FFmpeg.Gui/Controls/VideoScaleInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FFmpeg.Gui.Controls
+{
+    public class VideoScaleInput: Control
+    {
+        private static readonly Regex _regex = new Regex("[^0-9.-]+");
+
+        public int VideoWidth
+        {
+            get { return (int)GetValue(VideoWidthProperty); }
+            set { SetValue(VideoWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty VideoWidthProperty =
+            DependencyProperty.Register("VideoWidth", typeof(int), typeof(VideoScaleInput), new PropertyMetadata(-1));
+
+        public int VideoHeight
+        {
+            get { return (int)GetValue(VideoHeightProperty); }
+            set { SetValue(VideoHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty VideoHeightProperty =
+            DependencyProperty.Register("VideoHeight", typeof(int), typeof(VideoScaleInput), new PropertyMetadata(-1));
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var widthBox = GetTemplateChild("PART_VideoWith") as TextBox;
+            var heightBox = GetTemplateChild("PART_VideoHeight") as TextBox;
+            widthBox.PreviewTextInput += RestrictTextInput;
+            heightBox.PreviewTextInput += RestrictTextInput;
+        }
+
+        private void RestrictTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = _regex.IsMatch(e.Text);
+        }
+    }
+}
