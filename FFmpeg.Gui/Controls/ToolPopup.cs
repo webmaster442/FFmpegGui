da3b806808cca155c285cff97ba538a8fe2f3d86
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-03-24 12:47:52 +0100

    Tool popup window

---
+++ FFmpeg.Gui/Controls/ToolPopup.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FFmpeg.Gui.Controls
+{
+    internal class ToolPopup : Control
+    {
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ToolPopup), new PropertyMetadata(string.Empty));
+
+
+        public object Content
+        {
+            get { return GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register("Content", typeof(object), typeof(ToolPopup), new PropertyMetadata(null));
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var closeButton = GetTemplateChild("PART_CLOSE") as Button;
+            closeButton.Click += CloseButton_Click;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Visibility = Visibility.Collapsed;
+        }
+    }
+}
